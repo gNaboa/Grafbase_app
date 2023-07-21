6ecacf518eec9b222d12c9a82e25530032959045
gNaboa <48036612+gNaboa@users.noreply.github.com>
2023-07-21 11:40:53 -0300

    update

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,6 +1,6 @@
 import { g, auth, config } from '@grafbase/sdk'
 
-
+//@ts-ignore
 const User = g.model('User',{
   name:g.string().length({min:2,max:20}),
   email:g.string().unique(),
@@ -10,8 +10,10 @@ const User = g.model('User',{
   linkedinUrl:g.string().optional(),
    projects:g.relation(()=>Project).list().optional() 
 
+}).auth((rules)=>{
+  rules.public().read()
 })
-
+//@ts-ignore
 const Project = g.model('Project',{
     title:g.string().length({min:3}),
     description:g.string(),
@@ -21,9 +23,21 @@ const Project = g.model('Project',{
    category:g.string().search(),
    createBy:g.relation(()=>User)
 
+}).auth((rules)=>{
+  rules.public().read(),
+  rules.private().create().delete().update()
+})
+ const jwt = auth.JWT({
+  issuer:'grafbase',
+  secret:g.env('NEXTAUTH_SECRET')
 })
 
 export default config({
-  schema: g
+  schema: g,
+  auth:{
+    providers:[jwt],
+    rules:(rules)=>rules.private()
+  }
+
 
 })
--- src/lib/session.ts
+++ src/lib/session.ts
@@ -3,6 +3,23 @@ import { AdapterUser } from "next-auth/adapters";
 
 import GoogleProvider from 'next-auth/providers/google'
 import { createUser, getUser } from "./action";
+import jsonwebtoken from 'jsonwebtoken'
+import { JWT } from "next-auth/jwt";
+interface ProjectInterface {
+    title: string;
+    description: string;
+    image: string;
+    liveSiteUrl: string;
+    githubUrl: string;
+    category: string;
+    id: string;
+    createdBy: {
+        name: string;
+        email: string;
+        avatarUrl: string;
+        id: string;
+    };
+}
 
 interface SessionInterface extends Session {
     user: User & {
@@ -12,7 +29,24 @@ interface SessionInterface extends Session {
         avatarUrl: string;
     };
 }
-
+interface UserProfile {
+    id: string;
+    name: string;
+    email: string;
+    description: string | null;
+    avatarUrl: string;
+    githubUrl: string | null;
+    linkedinUrl: string | null;
+    projects: {
+        edges: { node: ProjectInterface }[];
+        pageInfo: {
+            hasPreviousPage: boolean;
+            hasNextPage: boolean;
+            startCursor: string;
+            endCursor: string;
+        };
+    };
+}
 
 export const authOptions: NextAuthOptions = {
     secret: 'my-secret',
@@ -27,21 +61,44 @@ export const authOptions: NextAuthOptions = {
         colorScheme: 'light',
         logo: '/logo.svg'
     },
-    // jwt:{
-    //     decode:({secret,token})=>{
+    jwt: {
 
-    //     },
-    //     encode:async ({secret,token})=>{
+        encode: async ({ secret, token }) => {
+            const encodedToken = jsonwebtoken.sign({
+                ...token,
+                iss: 'grafbase',
+                exp: Math.floor(Date.now() / 1000) + 60 * 60
+            }, secret)
+            return encodedToken
+        },
 
-    //     }
+        decode: ({ secret, token }) => {
+            const decodedToken = jsonwebtoken.verify(token!,secret) as JWT
+            return decodedToken
 
+        }
 
+    },
     // },
     callbacks: {
         async session({ session }) { // get triggred everytime a user visits the page
+            const email = session.user?.email as string
+            try {
+                const data = await getUser(email) as { user: UserProfile }
+                const newSession = {
+                    ...session,
+                    user: {
+                        ...session.user,
+                        ...data.user
+                    }
+                }
+                return newSession
+            } catch (e) {
+                console.log('session error: ', e)
+                return session
+            }
 
 
-            return session
 
         },
         async signIn({ user }: { user: AdapterUser | User }) {
@@ -50,7 +107,7 @@ export const authOptions: NextAuthOptions = {
                 const userExists = await getUser(user?.email as string)
                 if (!userExists) {
                     await createUser(
-                        user.name as string, 
+                        user.name as string,
                         user.email as string,
                         user.image as string)
                 }
